cfe64252788e1ffd2966b3278aabea83dd489d9b
Wenderson Guedes <wendersonguedes6@gmail.com>
2023-09-14 11:08:25 -0300

    implementando funcionalidade de filtragem de usuário de acordo com seu nivel de permissão

--- app/Livewire/UsersTable.php
+++ app/Livewire/UsersTable.php
@@ -11,13 +11,20 @@ class UsersTable extends Component
     use WithPagination;
 
     public $search = '';
-
     public $perPage = 5;
+
+    public $admin = '';
 
     public function render()
     {
         return view('livewire.users-table', [
-            'users' => User::search($this->search)->paginate($this->perPage),
+            'users' => User::search($this->search)
+                /* se $this->$admin for uma string vazia, não será feita uma pesquisa, então mostrará todos os usuários */
+                ->when($this->admin !== '', function ($query) {
+                    dd("entrou");
+                    $query->where('is_admin', $this->admin);
+                })
+                ->paginate($this->perPage),
         ]);
     }
 }
--- resources/views/livewire/users-table.blade.php
+++ resources/views/livewire/users-table.blade.php
@@ -26,6 +26,7 @@
                             <div class="flex space-x-3 items-center">
                                 <label class="w-40 text-sm font-medium text-gray-900">User Type :</label>
                                 <select
+                                    wire:model.live='admin'
                                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
                                     <option value="">All</option>
                                     <option value="0">User</option>
