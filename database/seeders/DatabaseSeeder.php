f39a3a4402fb793defd0aa3273539b067580a1c8
wenderson guedes <wendersonguedes6@gmail.com>
2023-09-17 16:44:16 -0300

    criando 100 usuários fakes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        //  \App\Models\User::factory(100)->create();
+        \App\Models\User::factory(100)->create();
 
-        \App\Models\User::factory()->create([
-            'name' => 'Wenderson Guedes',
-            'email' => 'wenderson@gmail.com',
-        ]);
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Wenderson Guedes',
+        //     'email' => 'wenderson@gmail.com',
+        // ]);
     }
 }
